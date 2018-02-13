74f0c5bf5601ab3754ec9edb7e84c5df26629f4b
Shi Rong <shi_rong_1994@yahoo.com.sg>
2018-02-14 01:52:48 +0800

    Updated expected test results

--- test/runtests.bat
+++ test/runtests.bat
@@ -10,7 +10,7 @@ REM compile the code into the bin folder
 javac  -cp ..\src -Xlint:none -d ..\bin ..\src\seedu\addressbook\Main.java
 
 REM run the program, feed commands from input.txt file and redirect the output to the actual.txt
-java -classpath ..\bin seedu.addressbook.Main < input.txt > actual.txt
+java --add-modules java.se.ee -classpath ..\bin seedu.addressbook.Main < input.txt > actual.txt
 
 REM compare the output to the expected output
 FC actual.txt expected.txt
