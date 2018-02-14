35569f1c0383b61af5b9a556a5daf6f29ee7bf6c
Shi Rong <shi_rong_1994@yahoo.com.sg>
2018-02-14 12:53:05 +0800

    Fix errors

--- test/runtests.sh
+++ test/runtests.sh
@@ -16,7 +16,7 @@ fi
 javac -cp ../src -Xlint:none -d ../bin ../src/seedu/addressbook/Main.java
 
 # run the program, feed commands from input.txt file and redirect the output to the actual.txt
-java -classpath ../bin seedu.addressbook.Main < input.txt > actual.txt
+java --add-modules java.se.ee -classpath ../bin seedu.addressbook.Main < input.txt > actual.txt
 
 # compare the output to the expected output
 diff actual.txt expected.txt
